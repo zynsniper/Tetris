55bec68499b537f881d93d4373c57a7814d5e8e2
zynsniper <cheang5337@gmail.com>
2025-05-10 23:33:41 -0300

    Added colours, generateRandomPiece() & renderPiece()

--- script.js
+++ script.js
@@ -35,3 +35,45 @@ const SHAPES = [
         [0,1,0]
     ]
 ]
+
+const COLOURS = [
+    "#0441ae",
+    "#72cb3b",
+    "#ffd504",
+    "#ff971d",
+    "#ff3215",
+    "#80014d",
+    "#569fb1"
+]
+
+const Rows = 20;
+const Cols = 10;
+
+let canvas = document.querySelector("#tetris");
+let ctx = canvas.getContext("2d");
+ctx.scale(30,30);
+
+var pieceObj = generateRandomPiece();
+console.log(pieceObj);
+function generateRandomPiece(){
+    let random = Math.floor(Math.random() * SHAPES.length);
+    let piece = SHAPES[random];
+    let pieceColour = COLOURS[random];
+    let x = 4;
+    let y = 0;
+
+    return {piece, x, y, pieceColour};
+}
+renderPiece();
+function renderPiece(){
+    let piece = pieceObj.piece;
+    ctx.fillStyle = pieceObj.pieceColour;
+
+    for(let i = 0; i < piece.length; i++){
+        for(let j = 0; j < piece[i].length; j++){
+            if(piece[i][j] == 1){
+                ctx.fillRect(j, i, 1, 1);
+            }
+        }
+    }
+}
